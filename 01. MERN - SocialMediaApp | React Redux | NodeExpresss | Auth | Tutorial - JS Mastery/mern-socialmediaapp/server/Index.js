87a3977da651ac9137206ba5b98e36353afe4e44
Daniel Bauer <dan717@gmx.de>
2023-06-10 14:27:42 +0200

    Test push 9

--- 01. MERN - SocialMediaApp | React Redux | NodeExpresss | Auth | Tutorial - JS Mastery/mern-socialmediaapp/server/Index.js
+++ 01. MERN - SocialMediaApp | React Redux | NodeExpresss | Auth | Tutorial - JS Mastery/mern-socialmediaapp/server/Index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 
 const app = expresss();
 
-// MW Parsing bodies
+// MW Parsing bodies:
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 // MW for CORS:
