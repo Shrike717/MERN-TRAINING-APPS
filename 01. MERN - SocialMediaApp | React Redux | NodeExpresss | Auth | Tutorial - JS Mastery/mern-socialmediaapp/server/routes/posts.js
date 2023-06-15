a42c6ceaf2feb4b64c55f6fdc1d97a4514ebfbc0
Daniel Bauer <dan717@gmx.de>
2023-06-15 12:07:38 +0200

    Done editing post

--- 01. MERN - SocialMediaApp | React Redux | NodeExpresss | Auth | Tutorial - JS Mastery/mern-socialmediaapp/server/routes/posts.js
+++ 01. MERN - SocialMediaApp | React Redux | NodeExpresss | Auth | Tutorial - JS Mastery/mern-socialmediaapp/server/routes/posts.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getPosts, createPost } from "../controllers/posts.js";
+import { getPosts, createPost, updatePost } from "../controllers/posts.js";
 
 // Setting up router
 const router = express.Router();
@@ -8,5 +8,6 @@ const router = express.Router();
 // http://localhost:5000/posts -> Prefix!
 router.get("/", getPosts);
 router.post("/", createPost);
+router.patch("/:id", updatePost); // Edit 1. Route
 
 export default router;
