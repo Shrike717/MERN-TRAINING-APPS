10f0f0ddb7de3dc19a7041f9fd72f1d23615f95e
Daniel Bauer <dan717@gmx.de>
2023-07-22 12:01:28 +0200

    Added cuustom signup

---
+++ 02. MERN - CouchSurfingApp | MERN | Firebase | Mapbox | JWT | Google Auth/place-to-stay/client/src/components/actions/user.js
@@ -0,0 +1,41 @@
+// Here we store  all user related functionality for communicating wiith BE:
+
+import {
+	CLOSE_LOGIN,
+	END_LOADING,
+	START_LOADING,
+	UPDATE_ALERT,
+	UPDATE_USER,
+} from "../../constants/actionTypes";
+
+import fetchData from "./utils/fetchData";
+
+const url = process.env.REACT_APP_SERVER_URL + "/user";
+
+export const register = async (user, dispatch) => {
+	dispatch({ type: START_LOADING });
+
+	// Send request with fetch:
+	const result = await fetchData(
+		{
+			url: url + "/register",
+			body: user, // Is name, email, password
+		},
+		dispatch
+	);
+	// If we receive an object in result we update the user:
+	if (result) {
+		dispatch({ type: UPDATE_USER, payload: result });
+		dispatch({ type: CLOSE_LOGIN }); // Close the sign p modal
+		dispatch({
+			type: UPDATE_ALERT,
+			payload: {
+				open: true,
+				severity: "success",
+				message: "Your account has been created successfully.",
+			},
+		});
+	}
+
+	dispatch({ type: END_LOADING });
+};
--- 02. MERN - CouchSurfingApp | MERN | Firebase | Mapbox | JWT | Google Auth/place-to-stay/server/controllers/user.js
+++ 02. MERN - CouchSurfingApp | MERN | Firebase | Mapbox | JWT | Google Auth/place-to-stay/server/controllers/user.js
@@ -6,21 +6,26 @@ import User from "../models/User.js";
 export const register = async (req, res) => {
 	try {
 		const { name, email, password } = req.body;
-		if (password.length < 6)
+		console.log(name, email, password);
+		if (password.length < 6) {
 			res.status(400).json({
 				success: false,
-				message: "Password must be at least 6 characters",
+				message: "Password must be at least 6 characters.",
 			});
+			return; // Otherwise app crashes because headrs are alrady sent
+		}
 		const emailToLowerCase = email.toLowerCase();
 		const existingUser = await User.findOne({ email: emailToLowerCase });
-		if (existingUser)
+		if (existingUser) {
 			res.status(400).json({
 				success: false,
-				message: "User already exists",
+				message: "User already exists.",
 			});
+			return;
+		}
 		// If we make it this far we can hash the password wih 12 salts:
 		const hashedPassword = await bcrypt.hash(password, 12);
-		// Creatin User in DB
+		// Creating User in DB
 		const user = await User.create({
 			name,
 			email: emailToLowerCase,
