9e4d6dc4cf51e7158180767734caa7ed9a3e509f
Daniel Bauer <dan717@gmx.de>
2023-08-01 18:49:12 +0200

    Building Radio buttons for price options

--- 02. MERN - CouchSurfingApp | MERN | Firebase | Mapbox | JWT | Google Auth/place-to-stay/client/src/components/addRoom/addImages/progressList/ProgressList.js
+++ 02. MERN - CouchSurfingApp | MERN | Firebase | Mapbox | JWT | Google Auth/place-to-stay/client/src/components/addRoom/addImages/progressList/ProgressList.js
@@ -1,40 +1,15 @@
-// import React from "react";
-// import { ImageList } from "@mui/material";
-// import ProgressItem from "./ProgressItem";
-
-// // The component receives the files and passes them one by one to the ProgressItem component
-// const ProgressList = ({ files }) => {
-// 	// console.log("2. This is the file coming in in ProgressList", files);
-// 	return (
-// 		<ImageList
-// 			rowHeight={250}
-// 			sx={{
-// 				// Overwriting styling so that ImageList is responsive
-// 				"&.MuiImageList-root": {
-// 					gridTemplateColumns:
-// 						"repeat(auto-fill, minmax(250px, 1fr))!important",
-// 				},
-// 			}}
-// 		>
-// 			{files.map((file, index) => (
-// 				<ProgressItem file={file} key={index} />
-// 			))}
-// 		</ImageList>
-// 	);
-// };
-
-// export default ProgressList;
-
-// Coode Tutorial
-import { ImageList } from "@mui/material";
 import React from "react";
+import { ImageList } from "@mui/material";
 import ProgressItem from "./ProgressItem";
 
+// The component receives the files and passes them one by one to the ProgressItem component
 const ProgressList = ({ files }) => {
+	// console.log("2. This is the file coming in in ProgressList", files);
 	return (
 		<ImageList
 			rowHeight={250}
 			sx={{
+				// Overwriting styling so that ImageList is responsive
 				"&.MuiImageList-root": {
 					gridTemplateColumns:
 						"repeat(auto-fill, minmax(250px, 1fr))!important",
