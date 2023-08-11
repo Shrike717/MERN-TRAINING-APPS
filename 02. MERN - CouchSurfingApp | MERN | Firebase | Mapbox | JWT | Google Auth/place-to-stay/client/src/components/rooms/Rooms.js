f1565b256ef34b737258da29bcabc94d3c8db590
Daniel Bauer <dan717@gmx.de>
2023-08-11 18:14:00 +0200

    Added image list in rooms component

--- 02. MERN - CouchSurfingApp | MERN | Firebase | Mapbox | JWT | Google Auth/place-to-stay/client/src/components/rooms/Rooms.js
+++ 02. MERN - CouchSurfingApp | MERN | Firebase | Mapbox | JWT | Google Auth/place-to-stay/client/src/components/rooms/Rooms.js
@@ -1,7 +1,92 @@
 import React from "react";
+import {
+	Avatar,
+	Card,
+	Container,
+	ImageList,
+	ImageListItem,
+	ImageListItemBar,
+	Rating,
+	Tooltip,
+} from "@mui/material";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+
+import { useValue } from "../../context/ContextProvider";
 
 const Rooms = () => {
-	return <div>Rooms</div>;
+	// Extracting the  rooms from global state
+	const {
+		state: { filteredRooms },
+		dispatch,
+	} = useValue();
+	return (
+		// Container centralizes the rooms list
+		<Container>
+			<ImageList
+				gap={12} // This adds vertically and horizontally 12px spacing between images
+				sx={{ mb: 8 }}
+			>
+				{filteredRooms.map((room) => (
+					<Card key={room._id}>
+						<ImageListItem
+							// Shall take full height of the row.
+							sx={{
+								height: "400px !important", // Originally: 100% !important
+								width: "100% !important", // Originally without width
+							}}
+						>
+							<ImageListItemBar // This is the bar at the top of the card
+								sx={{
+									background:
+										"linear-gradient(to bottom, rgba(0,0,0,0.7)0%, rgba(0,0,0,0.3)70%, rgba(0,0,0,0)100%)",
+								}}
+								title={
+									room.price === 0
+										? "Free Stay"
+										: "$" + room.price
+								}
+								actionIcon={
+									<Tooltip
+										title={room.uName}
+										sx={{ mr: "5px" }}
+									>
+										<Avatar src={room.uPhoto} />
+									</Tooltip>
+								}
+								position="top"
+							/>
+							<img
+								src={room.images[0]}
+								alt={room.title}
+								loading="lazy"
+								style={{ cursor: "pointer" }}
+							/>
+							<ImageListItemBar
+								title={room.title}
+								actionIcon={
+									<Rating
+										sx={{
+											color: "rgba(255, 255, 255, 0.8)",
+											mr: "5px",
+										}}
+										defaultChecked={3.5} // This is the default value with 3.5 stars
+										precision={0.5} // We can rate with 1/2 stars
+										emptyIcon={
+											<StarBorderIcon
+												sx={{
+													color: "rgba(255, 255, 255, 0.8)",
+												}}
+											/>
+										}
+									/>
+								}
+							/>
+						</ImageListItem>
+					</Card>
+				))}
+			</ImageList>
+		</Container>
+	);
 };
 
 export default Rooms;
